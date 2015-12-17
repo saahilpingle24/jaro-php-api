8621baa9d1228bf0c7947bfb3d334d940575773c
Saahil Pingle <saahilpingle24@users.noreply.github.com>
2015-12-16 19:31:43 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,8 @@
+include 'fetch.php';
+
 function function_db_connect() {	
 	try {
-	    $conn = new PDO('mysql:host='.getenv('dbendpoint').';dbname=p2schema', getenv('dbusername'), getenv('dbpassword'));
+	    $conn = new PDO('mysql:host='$GLOBALS['dec_endpoint']';dbname=p2schema', $GLOBALS['dec_username'], $GLOBALS['dec_password']);
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
 	    return $conn;
 	} catch(PDOException $e) {
